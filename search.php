3bdc9241eacd1faa9ef69f6aaf6304694f11a86f
gautamsood15 <gauty22@gmail.com>
2020-06-14 06:50:04 +0530

    Adding FancyBox CDN

--- search.php
+++ search.php
@@ -22,6 +22,8 @@
 <head>
 	<title>Welcome to Spider Engine</title>
 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -183,6 +185,8 @@
 		</div>
 
 	</div>
+
+	<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 
 	<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 
