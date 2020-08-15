4eba23b7250d4f1a8337b224a2d4c95c10ca3e22
gautamsood15 <gauty22@gmail.com>
2020-08-16 03:18:13 +0530

    complete function to get numbers

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -13,7 +13,12 @@
 			$query = $this->con->prepare("SELECT COUNT(*) as total FROM sites WHERE title LIKE :term OR url LIKE :term OR keywords LIKE :term OR description LIKE :term");
 		
 
-			$query->bindParam(":term", $term);
+			$searchTerm = "%". $term ."%";
+			$query->bindParam(":term", $searchTerm);
+			$query->execute();
+
+			$row = $query->fetch(PDO::FETCH_ASSOC);
+			return $row["total"];
 
 
 		}
