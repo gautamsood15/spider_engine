048ad5766eb2bbb2530b4224e57dcfed958ab12f
gautamsood15 <gauty22@gmail.com>
2020-08-19 07:45:35 +0530

    Crawling images

--- crawl.php
+++ crawl.php
@@ -6,6 +6,7 @@
 
 	$alreadyCrawled = array();
 	$crawling = array();
+	$alreadyFoundImages = array();
 
 
 	function linkExists($url) {
@@ -110,14 +111,30 @@
 		else {
 			echo "ERROR: Failed to insert $url<br>";
 		}	
+
+
+		$imageArray = $parser->getImages();
+		foreach($imageArray as $image){
+			$src = $image->getAttribute("src");
+			$alt = $image->getAttribute("alt");
+			$title = $image->getAttribute("title");
+
+			if (!$title && !$alt) {
+				continue;
+			}
+
+
+			$src = createLink($src, $url);
+
+			if(!in_array($src, $alreadyFoundImages)) {
+				$alreadyFoundImages[] = $src;
+
+				// Insert the image
+			}
+
+		}
 	
 	}
-
-
-
-	$imageArray
-
-
 
 	function followLinks($url) {
 
