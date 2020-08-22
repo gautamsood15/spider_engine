e85288a5e9b292161a16b5d0c5de017778d59fe6
gautamsood15 <gauty22@gmail.com>
2020-08-22 21:11:31 +0530

    Adding basic structure of google homepage (index.php)

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Spider Engine</title>
+</head>
+<body>
+	<div class="mainSection">
+
+		<div class="logoContainer">
+
+			<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+			
+		</div>
+
+		<dir class="searchContainer">
+
+			<input type="text" name="term" class="searchBox">
+			<input type="submit" value="Search" class="searchButton">
+
+		</dir>
+		
+
+	</div>
+
+</body>
+</html>
