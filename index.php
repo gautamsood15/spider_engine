f41f50c517e78fa3ec2d502d95e874ffef50729e
gautamsood15 <gauty22@gmail.com>
2020-08-27 17:41:38 +0530

    adding style center to the index  page + adding .wrapper.indexpage

--- index.php
+++ index.php
@@ -7,24 +7,28 @@
 
 </head>
 <body>
-	<div class="mainSection">
 
-		<div class="logoContainer">
+	<div class="wrapper indexPage">
 
-			<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-			
+		<div class="mainSection">
+
+			<div class="logoContainer">
+
+				<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+				
+			</div>
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+	
+					<input type="text" name="query" class="searchBox">
+					<input type="submit" value="Search" class="searchButton">
+	
+				</form>
+
+			</div>
 		</div>
-
-		<div class="searchContainer">
-
-			<form action="search.php" method="GET">
-				<input type="text" name="query" class="searchBox">
-				<input type="submit" value="Search" class="searchButton">
-			</form>
-
-		</div>
-		
-
 	</div>
 
 </body>
