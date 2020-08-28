f0d7beb6006b5c261c54acb13e3081904c20fab6
gautamsood15 <gauty22@gmail.com>
2020-08-28 17:59:54 +0530

    Adding search Page structure

--- search.php
+++ search.php
@@ -1,5 +1,47 @@
-<?php 
-	
-	echo $_GET["query"];
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Spider Engine</title>
 
- ?>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper">
+
+		<div class="header">
+
+			<div class="headerContent">
+
+				<div class="logoContainer">
+					
+					<a href="index.php">
+						<img src="assets/images/spider2.png">
+					</a>
+
+				</div>
+
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+						
+						<div class="searchBarContainer">
+								
+							<input class="searchBox" type="text" name="query">
+
+							<button>
+								Search
+							</button>
+
+						</div>
+
+					</form>
+					
+				</div>
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
