343804e568fd5829f74bdf00e77563fd928cde7d
gautamsood15 <gauty22@gmail.com>
2020-08-30 22:35:54 +0530

    Loading html website

--- crawl.php
+++ crawl.php
@@ -1,13 +1,14 @@
 <?php 
 	
-	include("classes/DomDocumentParser.php")
+	include("classes/DomDocumentParser.php");
 	
 	function followLinks($url) {
-		$parser = new DomDocumentParser($url)	
+		$parser = new DomDocumentParser($url);	
 
 	}
 
 	$startUrl = "http://www.reecekenney.com";
+	
 	followLinks($startUrl);
 
 ?>
