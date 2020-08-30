d6f27ba73b4c251fda3863a5c73ca0cdd72a5633
gautamsood15 <gauty22@gmail.com>
2020-08-31 00:56:13 +0530

    conversting relative links to absolute links

--- crawl.php
+++ crawl.php
@@ -4,6 +4,27 @@
 	
 	function createLink($src, $url) {
 
+		$scheme = parse_url($url)["scheme"]; 
+		$host = parse_url($url)["host"];
+
+		if (substr($src, 0, 2) == "//") {
+			$src = $scheme . ":" . $src;
+		}
+		else if (substr($src, 0, 1) == "/") {
+			$src = $scheme . "://" . $host . $src;
+		}
+		else if (substr($src, 0, 2) == "./") {
+			$src = $scheme . "://" . $host . dirname(parse_url($url)["path"]).substr($src, 1);
+		}
+		else if (substr($src, 0, 3) == "../") {
+			$src = $scheme . "://" . $host . "/" . $src;
+		}
+		else if (substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+			$src = $scheme . "://" . $host . "/" . $src;
+		}
+
+		return $src;
+
 	}
 
 
@@ -27,16 +48,16 @@
 			}
 
 
-			createLink($href, $url);
+			$href = createLink($href, $url);
 
 
-			//echo $href . "<br>";
+			echo $href . "<br>";
 
 		}
 
 	}
 
-	$startUrl = "http://www.bbc.com";
+	$startUrl = "https://en.wikipedia.org/wiki/Frontline_(American_TV_program)";
 	
 	followLinks($startUrl);
 
